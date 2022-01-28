09ea067e794bfb7c8aecdba8a0d1354b84ebb340
s.wag <sxxyxxn75@ewhain.net>
2022-01-29 00:29:04 +0900

    Commit Log

---
+++ WebViewKt/app/src/main/java/com/example/webviewkt/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.webviewkt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import com.example.webviewkt.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.settings.javaScriptEnabled = true // 자바 스크립트 허용
+        /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
+        binding.webView.webViewClient = WebViewClient()
+        binding.webView.webChromeClient = WebChromeClient()
+        /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
+        binding.webView.loadUrl("https://www.ewha.ac.kr") // 링크 주소 load
+
+    }
+
+    override fun onBackPressed() {
+        if(binding.webView.canGoBack())
+        { // 웹사이트에서 뒤로 갈 페이지가 존재한다면
+            binding.webView.goBack() // 웹사이트 뒤로가기
+        }
+        else
+        {
+            super.onBackPressed() // 본래의 백버튼 기능 수행 (android)
+        }
+    }
+}
